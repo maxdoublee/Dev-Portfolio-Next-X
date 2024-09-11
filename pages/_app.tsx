915c4a7cf4a086a1d3706e91609aeec283026359
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 12:16:01 -0400

    can i leave?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,16 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-      <div className="grid grid-cols-12 gap-6 px-5 pt-40 sm:px-20 md:px-32 lg:px-36 xl:px-48">
+      <div
+        className="grid grid-cols-12 gap-6 px-5 pt-40 sm:px-20 md:px-32 lg:px-36 xl:px-48"
+        style={{
+          backgroundImage: "url('/images/circuits.jpg')", // Circuit background image
+          backgroundSize: "cover", // Ensure the image covers the full background
+          backgroundPosition: "center", // Center the image
+          backgroundRepeat: "no-repeat", // Prevent repetition
+          minHeight: "100vh", // Make sure the background covers the full viewport height
+        }}
+      >
         {/* Sidebar */}
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
           <Sidebar />
