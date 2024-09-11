8b179f8448781dbdf4df030bb7cbe02e79d8c931
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 11:46:14 -0400

    more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,19 +10,20 @@ function MyApp({ Component, pageProps }) {
       <div
         className="grid grid-cols-12 gap-6 px-5 pt-40 sm:px-20 md:px-32 lg:px-36 xl:px-48"
         style={{
-          backgroundImage: "url('/images/circuits.jpg')", // Custom background image
-          backgroundSize: "cover", // Make sure it covers the whole page
-          backgroundPosition: "center", // Center the background
-          backgroundRepeat: "no-repeat", // Prevent background repetition
-          minHeight: "100vh" // Ensure the background covers the entire viewport height
+          backgroundImage: "url('/images/circuits.jpg')", // Keep the background image
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
         }}
       >
         {/* Sidebar */}
-        <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
+        <div className="h-full col-span-12 lg:col-span-3 p-4 text-base text-center bg-white dark:bg-dark-500 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
           <Sidebar />
         </div>
+
         {/* Main Content */}
-        <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-dark-500">
+        <div className="flex flex-col col-span-12 lg:col-span-9 overflow-hidden bg-white dark:bg-dark-500 rounded-2xl shadow-custom-light dark:shadow-custom-dark p-6">
           <Navbar />
           <Component {...pageProps} />
         </div>
