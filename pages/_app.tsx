890c33e8e2d853a5a7b33ddf7507317d04411909
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 11:18:38 -0400

    gradient gone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,13 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-      <div className="grid grid-cols-12 gap-6 px-5 pt-40 sm:px-20 md:px-32 lg:px-36 xl:px-48 bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
-        {/* // do this div style later (after putting the content) */}
-        <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
-          {/* //!sidebar */}
+      <div className="grid grid-cols-12 gap-6 px-5 pt-40 sm:px-20 md:px-32 lg:px-36 xl:px-48">
+        {/* Sidebar */}
+        <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
           <Sidebar />
         </div>
+        {/* Main Content */}
         <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-dark-500">
-          {/* //!navbar */}
           <Navbar />
           <Component {...pageProps} />
         </div>
