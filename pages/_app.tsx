2cfbcfd87e680efdcda5e1211c3a16dedd72a1cc
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 10:22:14 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
-      <div className="min-h-screen flex justify-center items-start py-10 bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
+      <div className="min-h-screen flex justify-center items-start pt-20 bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
         <div className="grid grid-cols-12 gap-6 w-full max-w-5xl px-5 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48">
           {/* Sidebar */}
           <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
