9ecbe2453e51a16a3ed42a6df651eb891d62a4f9
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-12 19:22:06 -0400

    nice

--- data.ts
+++ data.ts
@@ -3,13 +3,12 @@ import { FaServer } from "react-icons/fa";
 import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
 import { MdDeveloperMode } from "react-icons/md";
 import { IProject, Service, Skill } from "./types";
-import { ReactComponent as roboticarm } from 'react-icons/custom/roboticarm.svg';
-
+import RoboticArmIcon from '/roboticarm.svg';
 import { BsCircleFill } from "react-icons/bs";
 
 export const services: Service[] = [
   {
-    Icon: roboticarm,
+    Icon: RoboticArmIcon,
     title: "Frontend Development",
     about:
       "I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>   and <b>React.js</b> ",
--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withSvgr = require('next-svgr');
 
 module.exports = withSvgr({
   images: {
-    domains: ["sumitdey.netlify.app"], 
-  },
-  webpack(config) {
-    return config;
+    domains: ["sumitdey.netlify.app"],
   },
 });
