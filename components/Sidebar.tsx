218766fc5423b23300de80cd386798386345aa7c
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 21:26:55 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -69,24 +69,19 @@ const Sidebar = () => {
         </p>
       </div>
 
-      {/* Buttons - Adjust spacing and layout */}
-      <div className="flex flex-col items-center space-y-4 w-full mt-5">
-        <button
-          className="w-8/12 px-5 py-2 text-white bg-black rounded-full"
-          onClick={() => window.open("mailto:destim@rpi.edu")}
-        >
-          Email Me
-        </button>
-
-        <a
-          href="https://github.com/creator-username"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-8/12 px-5 py-2 mt-4 text-white bg-black rounded-full text-center"
-        >
-          Credit Project Creator
-        </a>
-      </div>
+      <button
+        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        onClick={() => window.open("mailto:code.sumax@gmail.com")}
+      >
+        Email me
+      </button>
+      <button
+        onClick={changeTheme}
+        className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "
+      >
+        {/* //TODO remove bg black */}
+        Toggle Theme
+      </button>
     </>
   );
 };
