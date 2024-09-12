c20943dfee70bf0b4425d1a8e95c8cc57907449f
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-11 21:45:59 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -81,7 +81,7 @@ const Sidebar = () => {
         onClick={() => window.open('https://github.com/creator-username', '_blank')}
         className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer hover:scale-105 focus:outline-none"
       >
-        Project Creator
+        Project
       </button>
     </>
   );
