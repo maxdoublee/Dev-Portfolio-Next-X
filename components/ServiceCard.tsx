dc812c219e8268399bd8fad8592c9d28d5e81e48
Max Destil <mdestil@Maxs-MacBook-Pro.local>
2024-09-14 21:02:59 -0400

    arden

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,30 +1,26 @@
-import { FunctionComponent } from 'react';
-import { Service } from '../types';
+import { FunctionComponent } from 'react'
+import { Service } from '../types'
+// import { motion } from 'framer-motion'
 
 const ServiceCard: FunctionComponent<{ service: Service }> = ({
    service: { Icon, title, about },
 }) => {
-   // Create markup for the 'about' section
+   //XSS attack :( on our portfolio btw, as an alternate use npm i dom purify
    function createMarkup() {
       return {
          __html: about,
-      };
-   };
+      }
+   }
 
    return (
       <div className='flex items-center p-2 space-x-4 '>
-         {/* Adjust icon size using Tailwind CSS classes */}
-         <Icon className='w-12 h-12 text-green' /> {/* Change w-12 h-12 to adjust size */}
-         
-         {/* Or adjust icon size using inline styles */}
-         {<Icon style={{ width: '32px', height: '32px' }} className="text-green" /> }
-         
-         <div>
+         <Icon className='w-12 h-12 text-green' />
+         <div className=''>
             <h6 className='font-bold'>{title}</h6>
             <p dangerouslySetInnerHTML={createMarkup()} />
          </div>
       </div>
-   );
+   )
 }
 
-export default ServiceCard;
+export default ServiceCard
